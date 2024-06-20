9f154887aa204e80fb53a170f082490cec4aed3b
Dario-Cabrera01~ <dario.cabrera17@gmail.com>
2024-06-20 16:23:02 -0300

    Se mejora Navbar con los botones para inicio, update y fin de sesion

---
+++ js/shop.js
@@ -0,0 +1,11 @@
+const logoMiCuenta = document.getElementById("logoMiCuenta");
+const dropdownMiCuenta = document.getElementById("dropdownMiCuenta");
+
+logoMiCuenta.addEventListener("click", function () {
+  dropdownMiCuenta.style.display = dropdownMiCuenta.style.display === "block" ? "none" : "block";
+});
+document.addEventListener("click", function (event) {
+  if (!logoMiCuenta.contains(event.target)) {
+    dropdownMiCuenta.style.display = "none";
+  }
+});
